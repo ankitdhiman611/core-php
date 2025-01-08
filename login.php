cdf1f1c796aa7b4b0b19d3cc0442b04ed037a247
Ankit Dhiman <ankitdhiman@Ankits-MacBook-Air.local>
2025-01-08 22:00:26 +0530

    create signup page using php

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <h1>Login</h1>
+    <form action="login.php" method="post">
+        Enter your email id :<input type="email" name="email" required><br><br>
+        Enter your password :<input type="password" name="password" required><br><br>
+        <button>Login</button><br><br>
+        <a href="newuser.php">Create an Account..</a><br><br>
+        <a href="forgotpass.php">Forgot Pasword..</a>
+
+
+        
+
+    </form>
+
+
+    </center>
+    
+</body>
+</html>
